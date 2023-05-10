7884f1bbdc9ffdb7402f61e91cb16b0c33a318d8
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-05-10 12:25:08 +0100

    Basic Matter.js setup, new page layout

--- src/main.js
+++ src/main.js
@@ -1,34 +1,55 @@
+import Matter from 'matter-js'
+
 /*
-Starter JS
-----------
-
-Starter template for JavaScript web app projects.
-
-(Shaun A. Noordin | shaunanoordin.com | 20180413)
+Primary App Class
  */
-
-import { ImportExample } from "@src/importExample.js";
-
-/*  Primary App Class
- */
-//==============================================================================
 class App {
   constructor() {
-    let importExample = new ImportExample("HI THERE");
+    // module aliases
+    var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
 
-    this.console = document.getElementById("console");
-    this.console.innerHTML =
-      "This is a starter template for JS projects. <br>" +
-      importExample.getText();
+    // create an engine
+    var engine = Engine.create();
+
+    // create a renderer
+    var render = Render.create({
+      element: document.querySelector('#app main'),
+      engine: engine,
+      options: {
+        width: 600,
+        height: 800,
+      }
+    });
+
+    // create two boxes and a ground
+    var boxA = Bodies.rectangle(400, 200, 80, 80);
+    var boxB = Bodies.rectangle(450, 50, 80, 80);
+    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+    // add all of the bodies to the world
+    Composite.add(engine.world, [boxA, boxB, ground]);
+
+    // run the renderer
+    Render.run(render);
+
+    // create runner
+    var runner = Runner.create();
+
+    // run the engine
+    Runner.run(runner, engine);
+
+    console.log('+++ ', runner)
   }
 }
-//==============================================================================
 
-/*  Initialisations
+/*
+Initialisations
  */
-//==============================================================================
 var app;
 window.onload = function() {
   window.app = new App();
 };
-//==============================================================================
