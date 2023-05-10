ac97fe82b3710e3120fdbc1fb3f4cf3b052030b1
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2023-05-10 13:22:04 +0100

    Refactor

--- src/main.js
+++ src/main.js
@@ -5,51 +5,49 @@ Primary App Class
  */
 class App {
   constructor() {
-    // module aliases
-    var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+    const {
+      Engine, Render, Runner, Bodies, Composite
+     } = Matter
+    
+    this.canvasWidth = 1000
+    this.canvasHeight = 600
+    this.hero = null
 
-    // create an engine
-    var engine = Engine.create();
+    // Create Matter.js engine
+    this.matterJsEngine = Engine.create()
 
-    // create a renderer
-    var render = Render.create({
+    // Create Matter.js renderer
+    this.matterJsRender = Render.create({
       element: document.querySelector('#app main'),
-      engine: engine,
+      engine: this.matterJsEngine,
       options: {
-        width: 600,
-        height: 800,
+        width: this.canvasWidth,
+        height: this.canvasHeight,
+        showCollisions: true,
+        showPositions: true,
+        showAngleIndicator: true,
       }
-    });
+    })
 
-    // create two boxes and a ground
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    // Create objects
+    var boxA = Bodies.rectangle(400, 200, 80, 80)
+    var boxB = Bodies.rectangle(450, 50, 80, 80)
+    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
 
-    // add all of the bodies to the world
-    Composite.add(engine.world, [boxA, boxB, ground]);
+    // Add objects to world
+    Composite.add(this.matterJsEngine.world, [boxA, boxB, ground])
 
-    // run the renderer
-    Render.run(render);
-
-    // create runner
-    var runner = Runner.create();
-
-    // run the engine
-    Runner.run(runner, engine);
-
-    console.log('+++ ', runner)
+    // Run the engine!
+    Render.run(this.matterJsRender)
+    this.matterJsRunner = Runner.create()
+    Runner.run(this.matterJsRunner, this.matterJsEngine)
   }
 }
 
 /*
 Initialisations
  */
-var app;
+var app
 window.onload = function() {
-  window.app = new App();
-};
+  window.app = new App()
+}
